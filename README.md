76ba818ed4e37454eebb2e5bc1b39e322c656737
Ko Htut <fellinglove8@gmail.com>
2020-08-28 14:24:52 +0630

    fix : md

--- README.md
+++ README.md
@@ -4,10 +4,32 @@
   <img width="800" src="https://user-images.githubusercontent.com/4608155/74476584-77155300-4e5e-11ea-88c6-6c9f64cf0f05.png" alt="Octicons cover" />
 </p>
 
-## Guide
+## Install & Set up
 
+1. Add dependency, open the root directory `pubspec.yaml`File in`dependencies:`Add the following code below
+
+ ```yaml
+ octicons: #latest
+ ```
+
+2. Installation dependencies (please ignore if it has been installed automatically)
+
+```dart
+ cd Project directory
+ flutter packages get
+ ```
+
+3. Introduce the library in the page
+
+```dart
+import 'package:octicons/octicons.dart';
+```
+## Usage
+```dart
+Icon(Octicons.person),
+```
 ## Credit
-
+ - Flutter Icon ["https://www.fluttericon.com"]
 ## License
 
 MIT
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:octicons/octicons.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,15 +12,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Octicons App Bar'),
         ),
-        body: Column(
-          children: [
-            Icon(Octicons.alert),
-            Icon(Octicons.archive),
-            Icon(Octicons.arrow_both),
-            Icon(Octicons.arrow_down),
-            Icon(Octicons.arrow_right)
-          ],
-        )
+        body: Center(
+          child: Icon(
+            Octicons.person,
+            color: Colors.green,
+          ),
+        ),
       ),
     );
   }
